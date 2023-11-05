1d2a4227e59b02eb52941fb7b5b5ab1eb0d379e7
Viktorzee <monsieurviktor@yahoo.com>
2023-11-05 23:30:51 +0100

    database trial

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    },
+    connection: process.env.DB_URL,
     migrations: {
       directory: './src/migrations',
     },
