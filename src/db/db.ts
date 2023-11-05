c42600d281d305ebc433a4f6fe7800e91dc81d65
Viktorzee <monsieurviktor@yahoo.com>
2023-11-05 20:27:21 +0100

    change postgres to url

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,12 +5,10 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const connectionString = process.env.DB_URL
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT), // PostgreSQL default port
+  connectionString,
 });
 
 export const query = async (text:string, params:any[] = []) => {
